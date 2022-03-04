dfad8172f2e678f9732875f08e5bc72bbd3c3819
bowowo11 <574402481@qq.com>
2022-03-04 23:28:50 +0800

    1

--- thumbnail.js
+++ thumbnail.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 let video = document.querySelector("#bilibilivideo");
-video.src ="//player.bilibili.com/player.html?aid=845396404&bvid=BV1f54y1j7X8&cid=341913923&page=1"
 
 
 
-})
+var 迷迭香="//player.bilibili.com/player.html?aid=845396404&bvid=BV1f54y1j7X8&cid=341913923&page=1" 
+
+var 猫中毒="//player.bilibili.com/player.html?aid=714893724&bvid=BV1FX4y1g7u8&cid=318542946&page=1"  
+var 高跟鞋 ="//player.bilibili.com/player.html?aid=502193304&bvid=BV1PN411X7QW&cid=311661670&page=1"  
+var  水母之歌="//player.bilibili.com/player.html?aid=673687696&bvid=BV1DU4y15767&cid=354558365&page=1"  
+var 小拉乱撞="//player.bilibili.com/player.html?aid=851975249&bvid=BV1SL4y1M7Ns&cid=518876519&page=1"  
+
+video.src = 迷迭香
