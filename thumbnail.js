b8b3e15845247c757a43fa9758931a9a17d75713
bowowo11 <574402481@qq.com>
2022-03-05 00:14:50 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,29 +1,28 @@
 function dianna() {
     let video = document.querySelector("#bilibilivideo");
     video.src = "//player.bilibili.com/player.html?aid=714893724&bvid=BV1FX4y1g7u8&cid=318542946&page=1"
-    alert(video.src)
+
 }
 
 function eileen() {
     let video = document.querySelector("#bilibilivideo");
     video.src = "//player.bilibili.com/player.html?aid=845396404&bvid=BV1f54y1j7X8&cid=341913923&page=1" 
-    alert(video.src)
+
 }
 
 function carol() {
     let video = document.querySelector("#bilibilivideo");
     video.src = "//player.bilibili.com/player.html?aid=502193304&bvid=BV1PN411X7QW&cid=311661670&page=1"
-    alert(video.src)
 }
 
 function ava() {
     let video = document.querySelector("#bilibilivideo");
     video.src = "//player.bilibili.com/player.html?aid=673687696&bvid=BV1DU4y15767&cid=354558365&page=1"
-    alert(video.src)
+
 }
 
 function bella() {
     let video = document.querySelector("#bilibilivideo");
     video.src = "//player.bilibili.com/player.html?aid=851975249&bvid=BV1SL4y1M7Ns&cid=518876519&page=1"
-    alert(video.src)
+
 }
