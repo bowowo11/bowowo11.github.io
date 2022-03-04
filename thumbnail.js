6e689d845aa15dcc2250deb4ff6e0f602fe4a1bf
bowowo11 <574402481@qq.com>
2022-03-05 00:07:32 +0800

    1

--- thumbnail.js
+++ thumbnail.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 let video = document.querySelector("#bilibilivideo");
+}); 
 
-var eileen="//player.bilibili.com/player.html?aid=845396404&bvid=BV1f54y1j7X8&cid=341913923&page=1" 
-var dianna="//player.bilibili.com/player.html?aid=714893724&bvid=BV1FX4y1g7u8&cid=318542946&page=1"  
-var carol ="//player.bilibili.com/player.html?aid=502193304&bvid=BV1PN411X7QW&cid=311661670&page=1"  
-var ava="//player.bilibili.com/player.html?aid=673687696&bvid=BV1DU4y15767&cid=354558365&page=1"  
-var bella="//player.bilibili.com/player.html?aid=851975249&bvid=BV1SL4y1M7Ns&cid=518876519&page=1"  
 
-video.src = eileen
+function dianna() {
+    let video = document.querySelector("#bilibilivideo");
+    video.src = "//player.bilibili.com/player.html?aid=845396404&bvid=BV1f54y1j7X8&cid=341913923&page=1"
+    alert(video.src)
+}
