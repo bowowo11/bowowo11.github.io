3be7e02d88772b33d3afbb32fb6aaf8e3e7095fa
bowowo11 <574402481@qq.com>
2022-03-04 22:46:11 +0800

    1

--- thumbnail.js
+++ thumbnail.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+let video = document.querySelector("#bilibilivideo");
+video.src ="//player.bilibili.com/player.html?aid=845396404&bvid=BV1f54y1j7X8&cid=341913923&page=1"
 
 
 
