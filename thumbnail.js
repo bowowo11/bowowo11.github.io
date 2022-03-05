1a1f71ac8ec481ac87bda22231b7f055fffb0c38
bowowo11 <574402481@qq.com>
2022-03-05 09:07:02 +0800

    1

--- thumbnail.js
+++ thumbnail.js
@@ -1,34 +1,34 @@
 function dianna() {
     let video = document.querySelector("#bilibilivideo");
     let pic = document.querySelector("#picture");
-    video.src = "http://player.bilibili.com/player.html?aid=714893724&bvid=BV1FX4y1g7u8&cid=318542946&page=1"
+    video.src = "//player.bilibili.com/player.html?aid=714893724&bvid=BV1FX4y1g7u8&cid=318542946&page=1"
     pic.src="嘉然.png"
 }
 
 function eileen() {
     let video = document.querySelector("#bilibilivideo");
     let pic = document.querySelector("#picture");
-    video.src = "http://player.bilibili.com/player.html?aid=845396404&bvid=BV1f54y1j7X8&cid=341913923&page=1" 
+    video.src = "//player.bilibili.com/player.html?aid=845396404&bvid=BV1f54y1j7X8&cid=341913923&page=1" 
     pic.src="乃琳.png"
 }
 
 function carol() {
     let video = document.querySelector("#bilibilivideo");
     let pic = document.querySelector("#picture");
-    video.src = "http://player.bilibili.com/player.html?aid=502193304&bvid=BV1PN411X7QW&cid=311661670&page=1"
+    video.src = "//player.bilibili.com/player.html?aid=502193304&bvid=BV1PN411X7QW&cid=311661670&page=1"
     pic.src="珈乐.png"
 }
 
 function ava() {
     let video = document.querySelector("#bilibilivideo");
     let pic = document.querySelector("#picture");
-    video.src = "http://player.bilibili.com/player.html?aid=673687696&bvid=BV1DU4y15767&cid=354558365&page=1"
+    video.src = "//player.bilibili.com/player.html?aid=673687696&bvid=BV1DU4y15767&cid=354558365&page=1"
     pic.src="向晚.png"
 }
 
 function bella() {
     let video = document.querySelector("#bilibilivideo");
     let pic = document.querySelector("#picture");
-    video.src = "http://player.bilibili.com/player.html?aid=851975249&bvid=BV1SL4y1M7Ns&cid=518876519&page=1"
+    video.src = "//player.bilibili.com/player.html?aid=851975249&bvid=BV1SL4y1M7Ns&cid=518876519&page=1"
     pic.src="贝拉.png"
 }
