33deaf3c5a1fbdc2cbbd32926f622513c2120688
bowowo11 <574402481@qq.com>
2022-03-07 19:29:14 +0800

    1

--- bilibili.js
+++ bilibili.js
@@ -24,39 +24,39 @@ function creatediv()
 	bgObj.style.height=sHeight + "px";
 	bgObj.style.zIndex = "10000";
 	document.body.appendChild(bgObj);
-		
+	
 	var msgObj=document.createElement("div");
 	msgObj.setAttribute("id","msgDiv");
 	msgObj.setAttribute("align","center");
 	msgObj.style.position = "absolute";
-    msgObj.style.left = "50%";
-    msgObj.style.background="rgba(250, 250, 250, 0.8)";
-    msgObj.style.marginLeft = "-200px" ;
-    msgObj.style.top = (document.documentElement.clientHeight/2+document.documentElement.scrollTop-252)+"px";
-    msgObj.style.width = msgw + "px";
-    msgObj.style.height =msgh + "px";
-    msgObj.style.zIndex = "10001";
-    msgObj.innerHTML = "<p>点击图片可以进入个人主页，点个关注再走吧！orz</p><br/><a href=\"javascript:void(0);\" onclick='popshow()'>已关注</a><a href=\"javascript:void(0);\" style=\"margin: 0px 0px 0px 20px;\"onclick='delWinD();return false;'>这就去</a><a href=\"index.html\"style=\"margin: 0px 0px 0px 20px;\">狠心拒绝</a><br><br><br><img src=\"picture/爱心.jpg\">";
-    document.body.appendChild(msgObj); 
+	msgObj.style.left = "50%";
+	msgObj.style.background="rgba(250, 250, 250, 0.8)";
+	msgObj.style.marginLeft = "-200px" ;
+	msgObj.style.top = (document.documentElement.clientHeight/2+document.documentElement.scrollTop-252)+"px";
+	msgObj.style.width = msgw + "px";
+	msgObj.style.height =msgh + "px";
+	msgObj.style.zIndex = "10001";
+	msgObj.innerHTML = "<p>点击图片可以进入个人主页，点个关注再走吧！orz</p><br/><a href=\"javascript:void(0);\" onclick='popshow()'>已关注</a><a href=\"javascript:void(0);\" style=\"margin: 0px 0px 0px 20px;\"onclick='delWinD();return false;'>这就去</a><a href=\"index.html\"style=\"margin: 0px 0px 0px 20px;\">狠心拒绝</a><br><br><br><img src=\"picture/爱心.jpg\">";
+	document.body.appendChild(msgObj); 
 }
 
 function delWinD()
 {
-   document.getElementById("bgDiv").style.display="none";
-   document.getElementById("msgDiv").style.display="none";
-   isshow=0;
+	document.getElementById("bgDiv").style.display="none";
+	document.getElementById("msgDiv").style.display="none";
+	isshow=0;
 }
 function show()
 {   
-    isshow=1;
-    if(!document.getElementById("msgDiv"))
-        creatediv();
-    else
-    {
-        document.getElementById("bgDiv").style.display="";
-        document.getElementById("msgDiv").style.display="";
-        document.getElementById("msgDiv").style.top=(document.documentElement.clientHeight/2+document.documentElement.scrollTop-252)+"px";
-    }  
+	isshow=1;
+	if(!document.getElementById("msgDiv"))
+		creatediv();
+	else
+	{
+		document.getElementById("bgDiv").style.display="";
+		document.getElementById("msgDiv").style.display="";
+		document.getElementById("msgDiv").style.top=(document.documentElement.clientHeight/2+document.documentElement.scrollTop-252)+"px";
+	}  
 }
 function popshow(){
 	document.getElementById("poppic").style.opacity="1"
